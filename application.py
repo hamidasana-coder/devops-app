6c84a27acdeb656a03065680aebab85dfb76d0b3
Hamida Sana <hamida.sana@auaf.edu.af>
2026-04-30 15:36:07 +0430

    Initial commit: Flask app with form

---
+++ application.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to my DevOps Project!"
+
+@app.route("/form", methods=["GET", "POST"])
+def form():
+    if request.method == "POST":
+        name = request.form.get("name")
+        return f"Hello, {name}!"
+    return render_template("form.html")
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
